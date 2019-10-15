a5e5e37e335b809880a22d298e39ae7ae6e7ab8c
ilhamsj <ilhamsaputrajati@gmail.com>
2019-10-15 13:58:43 +0700

    font awesome added

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,10 +6,39 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pusat Urusan Kematian Yogyakarta - PUKJ</title>
+    <link rel="stylesheet" href="{{ secure_url('css/app.css') }}">
 </head>
 <body>
+
+  <nav class="navbar navbar-expand-sm navbar-light bg-light">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+        aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsibleNavId">
+      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+          <div class="dropdown-menu" aria-labelledby="dropdownId">
+            <a class="dropdown-item" href="#">Action 1</a>
+            <a class="dropdown-item" href="#">Action 2</a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
     <main>
         @yield('content')
     </main>
+
+    <script src="{{ secure_url('js/app.js') }}"></script>
 </body>
 </html>

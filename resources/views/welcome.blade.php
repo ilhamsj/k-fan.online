da8b5b055e8b836b622e85cddd51ae5459dd9089
ilhamsj <ilhamsaputrajati@gmail.com>
2019-10-16 07:04:51 +0700

    welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,41 +3,33 @@
 @section('content')
 
     {{-- Product --}}
-    <section>
+    <section class="my-6">
         <div class="container">
             <div class="row h-100-kfan align-items-center">
-                <div class="col-12 col-md-6 ">
-                    <h1 class="text-primary display-1a">
-                        Kami hadir untuk meringankan beban bagi keluarga yang ditinggal.
-                    </h1>
-                    <p>
-                        Bergabung bersama Kfan sekarang !
-                    </p>
-                    <div class="my-4"></div>
-                    <a href="" class="btn btn-outline-primary btn-primary-kfan btn-block rounded-pill">
-                        Login
-                    </a>
-                    <a href="" class="btn btn-primary btn-block rounded-pill">
-                        Register
-                    </a>
+                <div class="col-12 col-md-5">
+                            <h1 class="text-primary display-1a">
+                                Kami hadir untuk meringankan beban bagi keluarga yang ditinggal.
+                            </h1>
+                            <p>
+                                Bergabung bersama Kfan sekarang !
+                            </p>
+                            <div class="my-4"></div>
+                            <a href="" class="btn btn-outline-primary btn-primary-kfan btn-block rounded-pill">
+                                Login
+                            </a>
+                            <a href="" class="btn btn-primary btn-block rounded-pill">
+                                Register
+                            </a>
                 </div>
             </div>
         </div>
     </section>
 
-    {{-- Promotional --}}
-    <section class="bg-image my-6">
-        <div class="container">
-            <div class="row h-50-kfan align-items-center">
-            </div>
-        </div>
-    </section>
-
     {{-- Service --}}
-    <section class="my-6">
+    <section class="my-6" id="service">
         <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-12 text-center">
+            <div class="row">
+                <div class="col-10 m-auto text-center mb-4">
                     <i class="fas fa-star fa-2x"></i>
                     <div class="my-2"></div>
                     <h2>OUR <span class="text-primary">SERVICE</span> </h2>
@@ -45,12 +37,12 @@
                         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos laborum, ut nulla dolore eveniet recusandae assumenda, ducimus odio
                     </p>
                 </div>
-                <div class="col-12 col-md mb-4">
-                    <div class="card shadow border-0" style="border-radius:1rem">
-                        <img style="border-radius: 1rem 1rem 0 0" class="card-img-top" src="https://images.unsplash.com/photo-1514414305862-17574326c3b4?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=232&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=80&w=348" alt="" srcset="">
+                <div class="col-12 col-md-4 mb-4">
+                    <div style="border-radius:1rem" class="card shadow-sm border-0">
+                        <img style="border-radius: 1rem 1rem 0 0" class="card-img-top" src="{{ secure_url('images/3-columns (1).jpg') }}" alt="" srcset="">
                         <div class="card-body">
-                            <h3 style="font-size: 1rem">
-                                <a href="">BRONZE</a>
+                            <h3>
+                                Paket Bronze
                             </h3>
                             <span class="text-muted">$50</span>
                             <p class="text-muted">
@@ -63,12 +55,12 @@
                          </div>
                     </div>
                 </div>
-                <div class="col-12 col-md mb-4">
-                    <div class="card border-0">
-                        <img class="card-img-top rounded" src="holder.js/285x190?random=yes&auto=yes&textmode=exact" alt="" srcset="">
-                        <div class="card-body px-2">
-                            <h3 style="font-size: 1rem">
-                                <a href="">BRONZE</a>
+                <div class="col-12 col-md-4 mb-4">
+                    <div style="border-radius:1rem" class="card shadow-sm border-0">
+                        <img style="border-radius: 1rem 1rem 0 0" class="card-img-top" src="{{ secure_url('images/3-columns (1).jpg') }}" alt="" srcset="">
+                        <div class="card-body">
+                            <h3>
+                                Paket Bronze
                             </h3>
                             <span class="text-muted">$50</span>
                             <p class="text-muted">
@@ -83,12 +75,12 @@
                          </div>
                     </div>
                 </div>
-                <div class="col-12 col-md mb-4">
-                    <div class="card border-0">
-                        <img class="card-img-top rounded" src="holder.js/285x190?random=yes&auto=yes&textmode=exact" alt="" srcset="">
-                        <div class="card-body px-2">
-                            <h3 style="font-size: 1rem">
-                                <a href="">BRONZE</a>
+                <div class="col-12 col-md-4 mb-4">
+                    <div style="border-radius:1rem" class="card shadow-sm border-0">
+                        <img style="border-radius: 1rem 1rem 0 0" class="card-img-top" src="{{ secure_url('images/3-columns (1).jpg') }}" alt="" srcset="">
+                        <div class="card-body">
+                            <h3>
+                                Paket Bronze
                             </h3>
                             <span class="text-muted">$50</span>
                             <p class="text-muted">
@@ -100,8 +92,29 @@
                                 Lorem ipsum dolor sit amet consectetur <br/> 
                             </p>
                             <a href="" class="btn btn-outline-primary btn-primary-kfan btn-block rounded-pill">
-                                    <i class="fa fa-arrow-circle-right" aria-hidden="true"></i>
-
+                                <i class="fa fa-arrow-circle-right" aria-hidden="true"></i>
+                            </a>
+                         </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-4 mb-4">
+                    <div style="border-radius:1rem" class="card shadow-sm border-0">
+                        <img style="border-radius: 1rem 1rem 0 0" class="card-img-top" src="{{ secure_url('images/3-columns (1).jpg') }}" alt="" srcset="">
+                        <div class="card-body">
+                            <h3>
+                                Paket Bronze
+                            </h3>
+                            <span class="text-muted">$50</span>
+                            <p class="text-muted">
+                                <i class="fa fa-check-circle text-primary" aria-hidden="true"></i>
+                                Lorem ipsum dolor sit amet consectetur <br/> 
+                                <i class="fa fa-check-circle text-primary" aria-hidden="true"></i>
+                                Lorem ipsum dolor sit amet consectetur <br/> 
+                                <i class="fa fa-check-circle text-primary" aria-hidden="true"></i>
+                                Lorem ipsum dolor sit amet consectetur <br/> 
+                            </p>
+                            <a href="" class="btn btn-outline-primary btn-primary-kfan btn-block rounded-pill">
+                                <i class="fa fa-arrow-circle-right" aria-hidden="true"></i>
                             </a>
                          </div>
                     </div>
@@ -154,12 +167,29 @@
                         Turut berduka cita atas meninggalnya
                     </p>
                 </div>
+                <div class="col-6 col-md mb-4">
+                    <img class="img-fluid rounded" data-src="holder.js/225x281?random=yes&auto=yes&textmode=exact" alt="">              
+                </div>
+                <div class="col-6 col-md mb-4">
+                    <img class="img-fluid rounded" data-src="holder.js/225x281?random=yes&auto=yes&textmode=exact" alt="">              
+                </div>
+                <div class="col-6 col-md mb-4">
+                    <img class="img-fluid rounded" data-src="holder.js/225x281?random=yes&auto=yes&textmode=exact" alt="">              
+                </div>
+                <div class="col-6 col-md mb-4">
+                    <img class="img-fluid rounded" data-src="holder.js/225x281?random=yes&auto=yes&textmode=exact" alt="">              
+                </div>
+                <div class="col-6 col-md mb-4">
+                    <img class="img-fluid rounded" data-src="holder.js/225x281?random=yes&auto=yes&textmode=exact" alt="">              
+                </div>
+
                 <div class="col-12 text-right">
                     <a href="#" class="btn btn-sm btn-primary shadow-sm">
                         Show more
                         <i class="fa fa-arrow-right" aria-hidden="true"></i>
                     </a>
                 </div>
+                
             </div>
         </div>
     </section>
@@ -184,7 +214,7 @@
         .h-100-kfan {min-height: 100vh}
         .h-50-kfan {min-height: 50vh}
         .bg-image {
-            background-image: url(https://images.unsplash.com/photo-1570528812862-9984124e7e22?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixlib=rb-1.2.1&q=80&w=1189);
+            background-image: url('/images/photo-1455819760800-d2aa223b237a.jpg');
             background-repeat: no-repeat;
             background-size: cover;
             background-attachment: fixed;
@@ -207,12 +237,25 @@
         .my-6 {
             margin: 0 0 20vh 0;
         }
+
+        .rounded-sm {
+            border-radius:1rem;
+        }
+
+        .rounded-img-sm {
+            'border-radius: 1rem 1rem 0 0;
+        }
         
     </style>
 @endpush
 @push('scripts')
     <script>
         console.log('helo');
-
+        $('#service').find('.card').hover(function () {
+                $(this).addClass('shadow');
+            }, function () {
+                $(this).removeClass('shadow');
+            }
+        );
     </script>
 @endpush

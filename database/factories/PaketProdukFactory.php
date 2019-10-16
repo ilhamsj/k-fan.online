4d424c17bae9ddb701d485a3b2b5ecaa026c3ddd
ilhamsj <ilhamsaputrajati@gmail.com>
2019-10-16 11:23:53 +0700

    paket produk rcfm

---
+++ database/factories/PaketProdukFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\PaketProduk;
+use Faker\Generator as Faker;
+
+$factory->define(PaketProduk::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
--- database/migrations/2019_10_16_042329_create_paket_produks_table.php
+++ database/migrations/2019_10_16_042329_create_paket_produks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProduksTable extends Migration
+class CreatePaketProduksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,8 @@ class CreateProduksTable extends Migration
      */
     public function up()
     {
-        Schema::create('produks', function (Blueprint $table) {
+        Schema::create('paket_produks', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name', 100);
             $table->timestamps();
         });
     }
@@ -27,6 +26,6 @@ class CreateProduksTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('produks');
+        Schema::dropIfExists('paket_produks');
     }
 }

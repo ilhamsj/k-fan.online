b981c2d48b70bf54e19f9527b59bd78041a830c9
ilhamsj <ilhamsaputrajati@gmail.com>
2019-10-16 11:21:25 +0700

    produk & paket rcfm

---
+++ database/factories/PaketFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Paket;
+use Faker\Generator as Faker;
+
+$factory->define(Paket::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/factories/ProdukFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Produk;
+use Faker\Generator as Faker;
+
+$factory->define(Produk::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});

e5fd2e297f8fbd6b32333f5b47926f1ac5b7ba7b
ilhamsj <ilhamsaputrajati@gmail.com>
2019-10-30 11:53:56 +0700

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -137,24 +137,24 @@
                 <div class="col-6 col-md-3 mb-4 align-self-start berita-duka">
                     <div class="card border-0">
                         <img class="card-img-top img-fluid rounded" src="{{ preg_match('/jpg|png|jpeg|gif/', $item->foto) ? $item->foto : 'https://i.ibb.co/Wt7rX7K/RDJ-Tony-Stark-min-1.jpg' }}" alt="" srcset="">
-                        <div class="card-img-overlay p-0 d-flex align-items-end">
-                            <div class="card-body rounded-bottom bg-light">
+                        {{-- <div class="card-img-overlay p-0 d-flex align-items-end"> --}}
+                            <div class="card-body rounded-bottom px-0">
                                     <strong>
                                         <a href="">{{ $item->nama }}</a>
                                     </strong>
-                                    <div class="d-none d-md-block" style="font-size: 0.8rem">
+                                    <div class="" style="font-size: 0.8rem">
                                         <i class="fa fa-birthday-cake" aria-hidden="true"></i>
-                                        {{ \Carbon\Carbon::parse($item->lahir)->format('d M Y') }} -
-                                        {{ \Carbon\Carbon::parse($item->wafat)->format('d M Y') }}
+                                            {{ \Carbon\Carbon::parse($item->lahir)->format('d M Y') }} -
+                                            {{ \Carbon\Carbon::parse($item->wafat)->format('d M Y') }}
                                         <br>
                                         
-                                        <a target="_blank" href="https://www.google.co.in/maps/search/{{$item->alamat}}">
+                                        <a class="d-none d-md-block" target="_blank" href="https://www.google.co.in/maps/search/{{$item->alamat}}">
                                             <i class="fas fa-map-marked-alt"></i>
                                             {{ $item->alamat }}
                                         </a>
                                     </div>
                             </div>
-                        </div>
+                        {{-- </div> --}}
                     </div>
                 </div>
                 @endforeach

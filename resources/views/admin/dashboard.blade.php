b265a2b51cd762958e893e7cebd2080c2c78a6c8
ilhamsj <ilhamsaputrajati@gmail.com>
2019-11-03 20:52:57 +0700

    dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -103,7 +103,8 @@
                             datasets: [{
                                 label: 'Transaksi',
                                 data: data,
-                                backgroundColor: 'rgb(63, 101, 211, 0.5)'
+                                backgroundColor: 
+                                'rgba(255, 206, 86, 1)',
                             }]
                         }
                     };
@@ -136,7 +137,8 @@
                             datasets: [{
                                 label: 'Transaksi Status',
                                 data: data,
-                                backgroundColor: 'rgba(0, 119, 204, 0.3)'
+                                backgroundColor: 'rgba(75, 192, 192, 1)',
+
                             }]
                         }
                     };
@@ -170,14 +172,14 @@
                                 label: 'Paket Terlaris',
                                 data: data,
                                 // backgroundColor: 'rgba(0, 119, 204, 0.3)'
-                                backgroundColor: 'rgb(255, 99, 132)'
-                                // 'rgba(255, 206, 86, 0.2)'
-                                // 'rgba(255, 99, 132, 0.2)'
-                                // 'rgba(54, 162, 235, 0.2)'
-                                // 'rgba(255, 206, 86, 0.2)'
-                                // 'rgba(75, 192, 192, 0.2)'
-                                // 'rgba(153, 102, 255, 0.2)'
-                                // 'rgba(255, 159, 64, 0.2)'
+                                backgroundColor: 
+                                // 'rgb(255, 99, 132)',
+                                // 'rgba(255, 99, 132, 1)',
+                                'rgba(54, 162, 235, 1)',
+                                // 'rgba(255, 206, 86, 1)',
+                                // 'rgba(75, 192, 192, 1)',
+                                // 'rgba(153, 102, 255, 1)',
+                                // 'rgba(255, 159, 64, 1)'
                             }]
                         }
                     };

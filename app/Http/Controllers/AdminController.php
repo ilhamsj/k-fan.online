50236c72af6e3844e25f67e3d0736e0959338810
ilhamsj <ilhamsaputrajati@gmail.com>
2019-11-12 13:02:45 +0700

    middleware

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -8,7 +8,7 @@ class AdminController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth')->only('produk');
+        $this->middleware('auth');
         $this->middleware('admin');
     }
 
